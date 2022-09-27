44ac1d24b689f183d43449ec6aca4569914a85b5
Komla Adzam <adzamkomla.dev@gmail.com>
2022-09-27 23:58:58 +0000

    Setup validation and transformation

--- apps/main-service/src/main.ts
+++ apps/main-service/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { environment } from './environments/environment';
@@ -12,6 +12,13 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    })
+  );
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = environment.app.port;
