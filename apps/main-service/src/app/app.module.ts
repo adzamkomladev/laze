7b81bcb125606cdcad249016e688f681364c7498
Komla Adzam <adzamkomla.dev@gmail.com>
2022-09-27 23:55:54 +0000

    Setup db connection

--- apps/main-service/src/app/app.module.ts
+++ apps/main-service/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+import { environment } from '../environments/environment';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...environment.database,
+    } as TypeOrmModuleOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/main-service/src/environments/environment.ts
+++ apps/main-service/src/environments/environment.ts
@@ -1,3 +1,18 @@
 export const environment = {
   production: false,
+  app: {
+    name: process.env.MAIN_SERVICE_APP_NAME,
+    port: process.env.MAIN_SERVICE_APP_PORT,
+    url: process.env.MAIN_SERVICE_APP_URL,
+  },
+  database: {
+    type: process.env.MAIN_SERVICE_DATABASE_TYPE,
+    host: process.env.MAIN_SERVICE_DATABASE_HOST,
+    port: process.env.MAIN_SERVICE_DATABASE_PORT,
+    username: process.env.MAIN_SERVICE_DATABASE_USERNAME,
+    password: process.env.MAIN_SERVICE_DATABASE_PASSWORD,
+    name: process.env.MAIN_SERVICE_DATABASE_NAME,
+    synchronize: process.env.MAIN_SERVICE_DATABASE_SYNCHRONIZE === 'true',
+    autoLoadEntities: true,
+  },
 };
