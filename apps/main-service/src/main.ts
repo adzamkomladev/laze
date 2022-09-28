5e9c91c0c686fc999114b1a94b843137b77c3edb
Komla Adzam <adzamkomla.dev@gmail.com>
2022-09-28 09:30:18 +0000

    Add bull dashboard for jobs

--- apps/main-service/src/main.ts
+++ apps/main-service/src/main.ts
@@ -9,9 +9,36 @@ import { NestFactory } from '@nestjs/core';
 import { environment } from './environments/environment';
 
 import { AppModule } from './app/app.module';
+import { ExpressAdapter } from '@bull-board/express';
+import { Queue } from 'bull';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
+import { createBullBoard } from '@bull-board/api';
+import expressBasicAuth = require('express-basic-auth');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const serverAdapter = new ExpressAdapter();
+  serverAdapter.setBasePath('/bull-board');
+
+  const emailQueue = app.get<Queue>(`BullQueue_email`);
+  const smsQueue = app.get<Queue>(`BullQueue_sms`);
+
+  createBullBoard({
+    queues: [new BullAdapter(emailQueue), new BullAdapter(smsQueue)],
+    serverAdapter,
+  });
+
+  app.use(
+    '/bull-board',
+    expressBasicAuth({
+      users: {
+        user: 'password',
+      },
+      challenge: true,
+    }),
+    serverAdapter.getRouter()
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
